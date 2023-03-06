c0c468905cd79a3ecba487f59121d8cf0665667b
João Silva <joao.silva2@unit4.com>
2023-03-06 10:30:31 +0000

    Starting closable tablist

--- src/components/tablist/ClosableTabList.js
+++ src/components/tablist/ClosableTabList.js
@@ -1,11 +1,47 @@
 import Tablist from "./tablist.js";
 
 export default class ClosableTabList extends Tablist {
-    _createTab(tabId, panelId, tabTitle) {
-        return `
-            <button id="${tabId}" aria-controls="${panelId}" role="tab" type="button" aria-selected="false">
-                ${tabTitle}
-            </button>
-        `;
+    _createTab(tabId, panelId, tabTitle, index) {
+      return `
+        <div id="${index}" style="display: inline-block; margin-right: 10px;">
+          <button id="${tabId}" aria-controls="${panelId}" role="tab" type="button" aria-selected="false">
+            ${tabTitle}
+          </button>
+          <button class="close-tab-button" data-index="${index}">
+            X
+          </button>
+        </div>
+      `;
+    }
+  
+    _addButtonEvents(index) {
+      const closeButton = this._tabs[index].querySelector('.close-tab-button');
+      closeButton.addEventListener('click', () => {
+        this.removeTab(index);
+        this.setActiveTab(index+1);
+      });
+    }
+  
+    removeTab(index) {
+      this._tabs.splice(index, 1);
+      this._panels.splice(index, 1);
+    }
+
+    addTab(tabTitle, panelParagraph) {
+        const index = this._tabs.length;
+        const tabId = `${this._anchorDom.id}_tab${index}`;
+        const panelId = `${this._anchorDom.id}_panel${index}`
+        
+        const tab = this._createTab(tabId, panelId, tabTitle,index);
+        const panel = this._createPanel(panelId, tabId, panelParagraph);
+
+        const tabNode = new DOMParser().parseFromString(tab, "text/html").body.firstElementChild;
+        const panelNode = new DOMParser().parseFromString(panel, "text/html").body.firstElementChild;
+        
+        this._tabs.push(tabNode);
+        this._panels.push(panelNode);
+        this._addTabEvents(index);
+        this._addButtonEvents(index);
+        //AddTabEvents is called last with index=0 so the active tab is set to the index 0 and that tab is not visible anymore
     }
 }
--- src/components/tablist/Tablist.js
+++ src/components/tablist/Tablist.js
@@ -1,23 +1,23 @@
 export default class Tablist {
-    #tabs = [];
-    #panels = [];
+    _tabs = [];
+    _panels = [];
     #activeTabIndex = -1;
-    #anchorDom = null;
+    _anchorDom = null;
 
     constructor(anchorDom) {
-        this.#anchorDom = anchorDom;
+        this._anchorDom = anchorDom;
     }
-
     setActiveTab(newActiveTabIndex = 0) {
-        if (this.#tabs === [] || newActiveTabIndex >= this.#tabs.length || newActiveTabIndex < 0)
+        if (this._tabs === [] || newActiveTabIndex >= this._tabs.length || newActiveTabIndex < 0)
              return;
-
+        
+        console.log(newActiveTabIndex)
         this.#activeTabIndex = newActiveTabIndex; 
-        this.#tabs.forEach((tab, index) => {
+        this._tabs.forEach((tab, index) => {
             tab.setAttribute('aria-selected', index === this.#activeTabIndex);
             tab.setAttribute('tabindex', (index == this.#activeTabIndex) ? 0 : -1);
         });
-        this.#panels.forEach((panel, index) => panel.style.setProperty('display', (index === this.#activeTabIndex) ? 'block' : 'none'));
+        this._panels.forEach((panel, index) => panel.style.setProperty('display', (index === this.#activeTabIndex) ? 'block' : 'none'));
     }
     
     #onBeforeRender() {
@@ -32,18 +32,16 @@ export default class Tablist {
 
         let newIndex = index;
         if (event.key === 'ArrowRight') {
-            newIndex = (index + 1) % this.#tabs.length;
+            newIndex = (index + 1) % this._tabs.length;
         } else if (event.key === 'ArrowLeft') {
-            newIndex = (index - 1 + this.#tabs.length) % this.#tabs.length;
+            newIndex = (index - 1 + this._tabs.length) % this._tabs.length;
         }
-
         this.setActiveTab(newIndex);
-        this.#tabs[newIndex].focus();
+        this._tabs[newIndex].focus();
     }
 
-    #addTabEvents(index) {
-        const tabNode = this.#tabs[index];
-
+    _addTabEvents(index) {
+        const tabNode = this._tabs[index];
         tabNode.onclick = () => this.setActiveTab(index);
         tabNode.onkeydown = (ev) => this.#addArrowNavigation(ev, index);
     }
@@ -56,7 +54,7 @@ export default class Tablist {
         `;
     }
 
-    #createPanel(panelId, tabId, panelParagraph) {
+    _createPanel(panelId, tabId, panelParagraph) {
         return `
             <div id="${panelId}" aria-labelledby="${tabId}" role="tabpanel" tabindex="0">
                 <p>${panelParagraph}</p>
@@ -65,19 +63,19 @@ export default class Tablist {
     }
 
     addTab(tabTitle, panelParagraph) {
-        const index = this.#tabs.length;
-        const tabId = `${this.#anchorDom.id}_tab${index}`;
-        const panelId = `${this.#anchorDom.id}_panel${index}`
+        const index = this._tabs.length;
+        const tabId = `${this._anchorDom.id}_tab${index}`;
+        const panelId = `${this._anchorDom.id}_panel${index}`
         
         const tab = this._createTab(tabId, panelId, tabTitle);
-        const panel = this.#createPanel(panelId, tabId, panelParagraph);
+        const panel = this._createPanel(panelId, tabId, panelParagraph);
 
         const tabNode = new DOMParser().parseFromString(tab, "text/html").body.firstElementChild;
         const panelNode = new DOMParser().parseFromString(panel, "text/html").body.firstElementChild;
         
-        this.#tabs.push(tabNode);
-        this.#panels.push(panelNode);
-        this.#addTabEvents(index);
+        this._tabs.push(tabNode);
+        this._panels.push(panelNode);
+        this._addTabEvents(index);
     }
 
     render() {
@@ -85,16 +83,16 @@ export default class Tablist {
 
         const tablistElement = document.createElement('div');
         tablistElement.setAttribute("role","tablist");
-        this.#tabs.forEach((tab) => {
+        this._tabs.forEach((tab) => {
             tablistElement.appendChild(tab);
         });
 
         const panelElement = document.createElement('div');
-        this.#panels.forEach((panel) => {
+        this._panels.forEach((panel) => {
             panelElement.appendChild(panel);
         });
 
-        this.#anchorDom.appendChild(tablistElement);
-        this.#anchorDom.appendChild(panelElement);
+        this._anchorDom.appendChild(tablistElement);
+        this._anchorDom.appendChild(panelElement);
     }
 }
