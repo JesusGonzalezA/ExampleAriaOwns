dd2d0c811cb1429e2d9ffe2375ee5b29c11d55bc
Jesús González Álvarez <Jesus.Gonzalez@unit4.com>
2025-03-26 10:54:43 +0100

    Fix aria-owns

--- src/components/tablist/ClosableWithAriaOwnsAndToolbar/ClosableAOWithToolbarTablist.js
+++ src/components/tablist/ClosableWithAriaOwnsAndToolbar/ClosableAOWithToolbarTablist.js
@@ -8,6 +8,7 @@ export default class ClosableAOWithToolbarTabList extends ClosableTabList {
 
     _renderTabList() {
       super._renderTabList();
+      this._tablistWrapperDom.removeAttribute('role');
       this.#updateAriaOwns();
     }
 
@@ -15,14 +16,19 @@ export default class ClosableAOWithToolbarTabList extends ClosableTabList {
       const closeButtonIdArr = this._tabs.map(tab => tab.getCloseButtonEl().id);
       const toolbarAriaOwns = [...closeButtonIdArr].join(' ');
       const toolbarNode = document.createElement('div');
+      const tablistWrapperDomWithAriaOwns = document.createElement('div');
+
       const toolbarId = `${this._anchorDom.id}_toolbar`
-      this._anchorDom.appendChild(toolbarNode);
       toolbarNode.setAttribute('id', toolbarId);
       toolbarNode.setAttribute('role', 'toolbar');
       toolbarNode.setAttribute('aria-owns', toolbarAriaOwns);
 
       const tabIdArr = this._tabs.map(tab => tab._getEl().id);
       const tablistAriaOwns = [...tabIdArr].join(' ');
-      this._tablistWrapperDom.setAttribute('aria-owns', tablistAriaOwns);
+      tablistWrapperDomWithAriaOwns.setAttribute('role', 'tablist');
+      tablistWrapperDomWithAriaOwns.setAttribute('aria-owns', tablistAriaOwns);
+
+      this._anchorDom.appendChild(tablistWrapperDomWithAriaOwns);
+      this._anchorDom.appendChild(toolbarNode);
     }
 }
